241370e6606f4e99683286d839237027d86a0197
Ronit <ronitkbiswas@gmail.com>
2025-12-05 17:46:00 +0530

    some changes

--- main.js
+++ main.js
@@ -15,6 +15,9 @@ async function getWeather() {
     result.textContent = "Enter a city please!";
     return;
   }
+  // city
+
+  // city ends
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(
     city
   )}&appid=${apiKey}&units=metric`;
@@ -27,7 +30,29 @@ async function getWeather() {
     const data = await res.json();
     const temp = data.main.temp;
     const desc = data.weather[0].description;
-    result.textContent = `${city}: ${temp}°C, ${desc}`;
+    const now = new Date();
+
+    let hours = now.getHours();
+    const minutes = now.getMinutes().toString().padStart(2, "0");
+    const ampm = hours >= 12 ? "PM" : "AM";
+
+    hours = hours % 12 || 12; // convert 24h → 12h format
+
+    const fullTime = `${hours}:${minutes} ${ampm}`;
+
+    result.innerHTML = `
+  <h2>${city}</h2>
+  <h3> ${fullTime} | ${temp}°C</h3>
+  <h3>${desc}</h3>
+`;
+    if (desc === "haze") {
+      document.body.style.backgroundColor = "lightgrey";
+      console.log("its hazey!");
+    } else if (desc === "scattered clouds") {
+      document.body.style.backgroundColor = "grey";
+    } else if (desc === "clear sky") {
+      document.body.style.backgroundColor = "skyblue";
+    }
   } catch (err) {
     result.textContent = "Something went wrong !";
     console.error(err);

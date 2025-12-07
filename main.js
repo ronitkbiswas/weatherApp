2adcb7060dfcb24dc2988f0258a40f0c89a96899
Ronit Kumar Biswas <ronitkbiswas@gmail.com>
2025-12-07 14:23:35 +0530

    Update main.js

--- main.js
+++ main.js
@@ -59,7 +59,7 @@ btn.addEventListener("click", async () => {
     }
     result.innerHTML = `
       <span style='font-size:22px'>${name}</span><br>
-          <span style='font-size:14px'>${fullTime}</span>| <span>${g}</span><br><br>
+          <span style='font-size:14px'>${fullTime}</span> | <span>${g}</span><br><br>
           <span style='font-size:40px'><b>${temp.toFixed(0)}°C</b></span>
         <br><br>
           <span>${desc}</span>
@@ -133,7 +133,7 @@ if (navigator.geolocation) {
         <span style='color:darkgreen;font-weight:500;'>Your current location</span>
         <br><br>
           <span style='font-size:22px'>${city}</span><br>
-          <span style='font-size:14px'>${fullTime}</span>| <span>${g}</span><br><br>
+          <span style='font-size:14px'>${fullTime}</span> | <span>${g}</span><br><br>
           <span style='font-size:40px'><b>${temp.toFixed(0)}°C</b></span>
         <br><br>
           <span>${desc}</span>
